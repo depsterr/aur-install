00c3ed8966b252ff4de8d540842521c95874fd68
depsterr <dragonvale09@gmail.com>
2019-11-09 02:01:48 +0100

    Create aur-install.sh

---
+++ aur-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+if [ -z "$1" ]
+then
+    echo "Error: no package specified"
+else
+    mkdir ~/.aur-get
+    rm ~/.aur-get/$1
+    git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-get/$1 && cd
+~/.aur-get/$1   && makepkg -Asrif --skipinteg && rm ~/.aur-get/$1 -rf
+fi
