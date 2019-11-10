796484b4a4b46892e88d82a80232dea8b447c5ce
depsterr <dragonvale09@gmail.com>
2019-11-10 02:49:44 +0100

    fixed some stuffs

--- aur-install.sh
+++ aur-install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 if [ -z "$1" ]
 then
-    echo "Error: no package specified"
+	echo "Error: no package specified"
 else
-    mkdir ~/.aur-get
-    rm ~/.aur-get/$1
-    git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-get/$1 && cd
-~/.aur-get/$1   && makepkg -Asrif --skipinteg && rm ~/.aur-get/$1 -rf
+	mkdir ~/.aur-get
+	rm ~/.aur-get/$1
+	git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-get/$1	&& cd ~/.aur-get/$1	&& makepkg -Asrif --skipinteg && rm ~/.aur-get/$1 -rf
 fi
