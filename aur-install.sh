7b6eb6d750234a2cad8becebe0a69de3f5d31979
depsterr <dragonvale09@gmail.com>
2019-11-24 17:27:17 +0100

    formatting and fixed directory

--- aur-install.sh
+++ aur-install.sh
@@ -3,7 +3,10 @@ if [ -z "$1" ]
 then
 	echo "Error: no package specified"
 else
-	mkdir ~/.aur-get
-	rm ~/.aur-get/$1
-	git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-get/$1	&& cd ~/.aur-get/$1	&& makepkg -Asrif --skipinteg && rm ~/.aur-get/$1 -rf
+	mkdir ~/.aur-install
+	rm ~/.aur-install/$1
+	git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-install/$1 \
+	&& cd ~/.aur-get/$1	\
+	&& makepkg -Asrif --skipinteg \
+	&& rm ~/.aur-install/$1 -rf
 fi
