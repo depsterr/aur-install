f1b156db61a9090cdb53946a8310371805c11d22
depsterr <depsterr@protonmail.com>
2020-02-27 00:09:07 +0100

    immensly improved script

--- aur-install.sh
+++ aur-install.sh
@@ -1,12 +1,24 @@
 #!/bin/sh
-if [ -z "$1" ]
-then
-	echo "Error: no package specified"
-else
-	mkdir ~/.aur-install
-	rm ~/.aur-install/$1 -rf
-	git clone `echo "https://aur.archlinux.org/"$1".git"` ~/.aur-install/$1 \
-	&& cd ~/.aur-install/$1	\
-	&& makepkg -Asrif --skipinteg \
-	&& rm ~/.aur-install/$1 -rf
-fi
+
+# No args
+[ -z "$1" ] && echo "Error: no package specified" && exit
+
+# Make sure aurrd variable is not set
+aurrd=""
+
+# Make sure the directory exists
+[ -d "$HOME/.aur-install" ] || mkdir "$HOME/.aur-install"
+[ -d "$HOME/.aur-install/$1" ] && \
+	read -p "It appears you have already downloaded this package, do you wish to redownload or install from disc? [r/d] (redownload/disc) " aurrd && \
+	[ "$aurrd" = "d" ] || rm "$HOME/.aur-install/$1" -rf
+
+# Don't redownload if disk option was selected
+[ "$aurrd" = "d" ] || git clone "https://aur.archlinux.org/$1.git" "$HOME/.aur-install/$1"
+
+# Install the package
+echo "Installing..."
+cd "$HOME/.aur-install/$1" && \
+	makepkg -Asrif --skipinteg
+
+# Clean up
+rm "$HOME/.aur-install/$1" -rf
